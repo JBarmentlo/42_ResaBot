4c56fe66998caa9210564e8ca5afbe43c9217960
Joep BARMENTLO <jbarment@student42.fr>
2021-03-26 16:16:32 +0100

    created PopUpclass and subscribe action and verification

---
+++ config.py
@@ -0,0 +1,17 @@
+class DotDict(dict):
+    """
+    a dictionary that supports dot notation 
+    as well as dictionary access notation 
+    usage: d = DotDict() or d = DotDict({'val1':'first'})
+    set attributes: d.val2 = 'second' or d['val2'] = 'second'
+    get attributes: d.val2 or d['val2']
+    """
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+config = DotDict()
+config.slot_times = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
+config.day_names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+config.accept_tdm = False
+config.preferred_floor = "E1"
