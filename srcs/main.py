389f5e16cb3327b9245dc6b2a0dc622d87e68660
Joep B <joepbarmentlo@gmail.com>
2021-03-30 22:40:53 +0200

    headless

--- srcs/main.py
+++ srcs/main.py
@@ -73,7 +73,8 @@ if __name__=="__main__":
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument('window-size=1024×768')
+    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument('window-size=1024×768')
 
     while (True):
         loop(cur)
